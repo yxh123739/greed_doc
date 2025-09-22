073777c56a190efbe359f9ba624a33209f936350
GreenDocTools <lilyduoc@outlook.com>
2025-09-22 10:02:37 -0400

    Update project-manager.tsx

--- components/project-manager.tsx
+++ components/project-manager.tsx
@@ -97,7 +97,7 @@ export function ProjectManager({
     return (
       <div className="flex items-center gap-1 text-sm text-muted-foreground">
         <Button variant="outline" asChild className="pr-0">
-          <Link href="/signin">Sign In To Save Your Project</Link>
+          <Link href="/signin">Sign In To Save Your Project! </Link>
         </Button>
       </div>
     );
